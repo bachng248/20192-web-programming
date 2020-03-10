adc381e9d3d2264128be971645f0934d94238862
bachng248 <ngb1998@gmail.com>
2020-03-10 10:29:47 +0700

    add week1

---
+++ week1/ex4/eg2.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Generating HTML from PHP</title>
+    </head>
+    <body>
+        <h1>Generating HTML from PHP</h1>
+        <?php
+            //
+            // Example script to output HTML tags
+            //
+        print("Using PHP has <i>some advantages:</i>");
+        print("<ul><li>speed</li><li>ease of use</li><li>functionality</li></ul>");
+        print("</body>
+              </html>");
+        ?>
