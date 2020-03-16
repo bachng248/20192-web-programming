666b0806a75c6a59d0c47c66aaabc068acdbe49b
bachng248 <ngb1998@gmail.com>
2020-03-16 14:04:21 +0700

    update week2

---
+++ week2/basicform.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>A Simple Form</title></head>
+    <body>
+        <form action="http://fit.hut.edu.vn/~trangntt/courses/vp" method="POST">
+            Click submit to start our initial PHP program.
+            <br>
+            <input type="text" name="Name">
+            <br>
+            <input type="submit" value="Click to submit">
+            <input type="reset" value="Erase and restart">  
+        </form>
+    </body>
+</html>
---
+++ week2/registrationform.php
@@ -0,0 +1,23 @@
+<html>
+    <head><title>Registration Form</title></head>
+    <body>
+        <form action="http://fit.hut.edu.vn/~trangntt/courses/wp" method="POST">
+            <h1>Registration Form</h1>
+            <label for="email">Email:</label>
+            <input type="email" name="email" value="">
+            <br>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" name="email" value="">
+            <br/>
+            <br>
+            <label for="name">Name:</label>
+            <input type="text" name="name" value="">
+            <br>
+            <br>
+            
+            <input type="submit" name="" value="Click to submit">
+            <input type="reset" name="" value="Erase and Restart">
+        </form>
+    </body>
+</html>
